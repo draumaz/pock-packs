9a4b75a8a38ab1fa9c47f94ab04271bf723c5dec
draumaz <draumaz@goatopossum.com>
2022-12-12 03:56:25 -0800

    fix some shit

--- candy/fastfetch/work/fastfetch-1.7.5/src/detection/wmtheme/wmtheme.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef FASTFETCH_INCLUDED_detection_wmtheme
-#define FASTFETCH_INCLUDED_detection_wmtheme
-
-bool ffDetectWmTheme(FFinstance* instance, FFstrbuf* themeOrError);
-
-#endif
